0afc3a7f509523dccca7df62fcaf930572867685
Hao Shen <hao.shen@cognizant.com>
2021-04-05 19:55:05 -0500

    updated build docker

--- build_dockerimages.sh
+++ build_dockerimages.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -e
-# docker system prune -a
-
 # Docker hub account hs888555 (bluehao85@gmail.com)
 $DOCKER_REGISTRY='hs888555'
 
 docker build --tag="kubermeter/jmeter-base:latest" -f jmeter-base.dockerfile .
 
-docker build --tag="hs888555/kubermeter-jmeter-master:latest" -f jmeter-master.dockerfile .
+docker build --tag="$DOCKER_REGISTRY/kubermeter-jmeter-master:latest" -f jmeter-master.dockerfile .
 docker push $DOCKER_REGISTRY/kubermeter-jmeter-master:latest
 
-docker build --tag="hs888555/kubermeter-jmeter-slave:latest" -f jmeter-slave.dockerfile .
+docker build --tag="$DOCKER_REGISTRY/kubermeter-jmeter-slave:latest" -f jmeter-slave.dockerfile .
 docker push $DOCKER_REGISTRY/kubermeter/jmeter-slave:latest
-s
