0ccd833ec2be0cd77aec996bdfef5525f49fbca2
Hao Shen <hao.shen@cognizant.com>
2021-04-06 10:33:26 -0500

    added create_jmeter_namespace

--- create_grafana_namespace.sh
+++ create_grafana_namespace.sh
@@ -2,20 +2,16 @@
 # Create a Grafana namespace on an existing kuberntes cluster
 
 working_dir=`pwd`
-GRAFANA_NAMESPACE='kubermeter-grafana'
-
-echo "checking if kubectl is present"
+GRAFANA_NAMESPACE=`awk -F= '/GRAFANA_NAMESPACE/{ print $2 }' ./kubermeter.properties`
 
 if ! hash kubectl 2>/dev/null
 then
-    echo "'kubectl' was not found in PATH"
-    echo "Kindly ensure that you can acces an existing kubernetes cluster via kubectl"
-    exit
+  echo "'kubectl' was not found in PATH"
+  echo "Kindly ensure that you can acces an existing kubernetes cluster via kubectl"
+  exit
 fi
 
-kubectl version --short
-
-#Check If namespace exists
+#Check If GRAFANA_NAMESPACE exists
 
 kubectl get namespace $GRAFANA_NAMESPACE > /dev/null 2>&1
 
@@ -24,8 +20,6 @@ then
   echo "Namespace '$GRAFANA_NAMESPACE' already exists. You don't need to create another one."
   exit 1
 fi
-
-exit
 
 echo
 echo "Creating Namespace: $GRAFANA_NAMESPACE"
@@ -50,5 +44,3 @@ echo
 
 kubectl get -n $GRAFANA_NAMESPACE all
 
-# Export to GRAFANA_NAMESPACE file
-$GRAFANA_NAMESPACE > $working_dir/GRAFANA_NAMESPACE
