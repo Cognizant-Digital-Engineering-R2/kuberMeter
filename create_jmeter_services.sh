e9637c1ee70adcc07124f9083a2e2cd9f84d0a98
Hao Shen <hao.shen@cognizant.com>
2021-04-06 11:36:43 -0500

    add jmeter slave num input

--- create_jmeter_services.sh
+++ create_jmeter_services.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Create multiple Jmeter namespaces on an existing kuberntes cluster
+# Create a new Jmeter namespace and resources on an existing kuberntes cluster
 #Started On January 23, 2018
 
 working_dir=`pwd`
@@ -13,22 +13,26 @@ kubectl get namespaces | grep -v NAME | awk '{print $1}'
 
 echo
 
-tenant="$1"
-if [ -z "$tenant" ]
-then
-  echo "Enter the name of the new tenant unique name, this will be used to create the namespace"
-  read tenant
-fi
+echo -n "Create a new namespace for the JMeter resources: $JMETER_NAMESPACE_PREFIX"
+read ns_input
+echo -n "How many JMeter slaves do you want to use? "
+read slave_num
+
+
+jmeter_namespace="$JMETER_NAMESPACE_PREFIX$ns_input"
+echo $jmeter_namespace
+echo $slave_num
+
 
 echo
 
 #Check If namespace exists
 
-kubectl get namespace $tenant > /dev/null 2>&1
+kubectl get namespace $jmeter_namespace > /dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
-  echo "Namespace $tenant already exists, please select a unique name"
+  echo "Namespace $jmeter_namespace already exists, please select a unique name"
   echo "Current list of namespaces on the kubernetes cluster"
   sleep 2
 
@@ -37,11 +41,13 @@ then
 fi
 
 echo
-echo "Creating Namespace: $tenant"
+echo "Creating Namespace: $jmeter_namespace"
 
-kubectl create namespace $tenant
+kubectl create namespace $jmeter_namespace
 
-echo "Namspace $tenant has been created"
+echo "Namspace $jmeter_namespace has been created"
+
+exit
 
 echo
 
@@ -57,16 +63,16 @@ echo
 
 echo "Creating Jmeter slave replicas and service"
 
-kubectl create -n $tenant -f $working_dir/jmeter_slaves.yaml
+kubectl create -n $jmeter_namespace -f $working_dir/jmeter_slaves.yaml
 
 echo "Creating Jmeter master"
 
-kubectl create -n $tenant -f $working_dir/jmeter_master.yaml
+kubectl create -n $jmeter_namespace -f $working_dir/jmeter_master.yaml
 
-echo "Printout Of the $tenant Objects"
+echo "Printout Of the $jmeter_namespace Objects"
 
 echo
 
-kubectl get -n $tenant all
+kubectl get -n $jmeter_namespace all
 
-echo namespace = $tenant > $working_dir/tenant_export
+echo namespace = $jmeter_namespace > $working_dir/tenant_export
