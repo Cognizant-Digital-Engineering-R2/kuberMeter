821a933cbda6c3a5609135785ed9c77683f29be1
Hao Shen <hao.shen@cognizant.com>
2021-04-08 06:32:48 +0800

    deleted jmeter_stop

--- create_jmeter_resources.sh
+++ create_jmeter_resources.sh
@@ -66,21 +66,21 @@ kubectl get -n $jmeter_namespace all
 
 echo
 
-## Create jmeter database automatically in Influxdb
+# ## Create jmeter database automatically in Influxdb
 
-echo "Creating Influxdb jmeter Database"
+# echo "Creating Influxdb jmeter Database"
 
-influxdb_pod=`kubectl get po -n $GRAFANA_NAMESPACE | grep influxdb-jmeter | awk '{print $1}'`
+# influxdb_pod=`kubectl get po -n $GRAFANA_NAMESPACE | grep influxdb-jmeter | awk '{print $1}'`
 
-kubectl exec -ti -n $GRAFANA_NAMESPACE $influxdb_pod -- influx -execute "CREATE DATABASE jmeter"
+# kubectl exec -ti -n $GRAFANA_NAMESPACE $influxdb_pod -- influx -execute "CREATE DATABASE jmeter"
 
 
-## Create the influxdb datasource in Grafana
+# ## Create the influxdb datasource in Grafana
 
-echo "Creating the Influxdb data source"
+# echo "Creating the Influxdb data source"
 
-grafana_pod=`kubectl get po -n $GRAFANA_NAMESPACE | grep jmeter-grafana | awk '{print $1}'`
+# grafana_pod=`kubectl get po -n $GRAFANA_NAMESPACE | grep jmeter-grafana | awk '{print $1}'`
 
-kubectl exec -ti -n $GRAFANA_NAMESPACE $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
+# kubectl exec -ti -n $GRAFANA_NAMESPACE $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"jmeterdb","type":"influxdb","url":"http://jmeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
 
-echo
+# echo
