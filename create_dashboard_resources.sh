f0be2b8402630db164e91e62a5cf4c0db570caf4
Hao Shen <hao.shen@cognizant.com>
2021-04-12 21:40:24 +0800

    added waiting logic for dashboard ip

--- create_dashboard_resources.sh
+++ create_dashboard_resources.sh
@@ -56,14 +56,14 @@ fi
 echo "Dashboard components are ready."
 echo
 
-## Create jmeter database automatically in Influxdb
+# Create jmeter database automatically in Influxdb
 echo "Creating Database 'jmeter' in influxdb..."
 influxdb_pod=`kubectl get po -n $DASHBOARD_NAMESPACE | grep influxdb-jmeter | awk '{print $1}'`
 kubectl exec -ti -n $DASHBOARD_NAMESPACE $influxdb_pod -- influx -execute "CREATE DATABASE jmeter"
 echo
 
 
-## Create the influxdb datasource in Grafana
+# Create the influxdb datasource in Grafana
 echo "Creating the data source 'jmeterdb' linking grafana to influxdb ..."
 grafana_pod=`kubectl get po -n $DASHBOARD_NAMESPACE | grep jmeter-grafana | awk '{print $1}'`
 kubectl exec -ti -n $DASHBOARD_NAMESPACE $grafana_pod -- curl 'http://admin:admin@127.0.0.1:3000/api/datasources' \
@@ -71,6 +71,33 @@ kubectl exec -ti -n $DASHBOARD_NAMESPACE $grafana_pod -- curl 'http://admin:admi
 '{"name":"jmeterdb","type":"influxdb","url":"http://kubermeter-influxdb:8086","access":"proxy","isDefault":true,"database":"jmeter","user":"admin","password":"admin"}'
 echo
 
+
+# "Wait for Grafana front-end external IP allocation
+waiting_msg="Waiting for Grafana front-end external IP allocation..."
+wait_time_elapsed="0"
+wait_time_interval="2"
+wait_time_max="60"
+start_time=$(date +%s)
 grafana_front_end_ip=`kubectl get svc -n $DASHBOARD_NAMESPACE | grep jmeter-grafana-frontend | awk '{print $4}'`
+
+while [[ "$grafana_front_end_ip" = '<pending>' ]]; do
+  
+  waiting_msg="${waiting_msg}."
+  echo -ne "$waiting_msg \r"
+  sleep $wait_time_interval
+  now=$(date +%s)
+  wait_time_elapsed=$(($now - $start_time))
+
+  if [[ $wait_time_elapsed -ge $wait_time_max ]]; then
+    echo "Grafana front-end external IP is not allocated within the limit of $wait_time_max seconds. Check the cluster health, \
+and/or use 'kubectl delete ns $DASHBOARD_NAMESPACE' to start over.\n"
+    exit 1
+  fi
+
+  grafana_front_end_ip=`kubectl get svc -n $DASHBOARD_NAMESPACE | grep jmeter-grafana-frontend | awk '{print $4}'`
+
+done
+
+echo
 echo "Dashboard resources created. Access http://$grafana_front_end_ip from a browser to import the 'kubermeter-dashboard.json'. \
 Select 'jmeterdb' for Data source."
